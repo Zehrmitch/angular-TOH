35f8374ae837c70bde48836cd33fda6c95b1ee9b
Mitchell Zehr <zehrmitch@gmail.com>
2020-11-19 19:37:30 -0500

    Tutorial Completion
    Finished section 6 of the tutorial

--- src/app/in-memory-data.service.spec.ts
+++ src/app/in-memory-data.service.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
-import { HeroService } from './hero.service';
 
-describe('HeroService', () => {
-  let service: HeroService;
+import { InMemoryDataService } from './in-memory-data.service';
+
+describe('InMemoryDataService', () => {
+  let service: InMemoryDataService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(HeroService);
+    service = TestBed.inject(InMemoryDataService);
   });
 
   it('should be created', () => {
